f97341bbfdf842470b748132686ec4970c559a17
Shams Tabrez <shamsshoaib261@gmail.com>
2023-06-02 21:17:20 +0530

    Create FindAvgMarks.py

---
+++ PythonBasicQuestion/FindAvgMarks.py
@@ -0,0 +1,9 @@
+# Write a program to input marks of three tests of a student (all integers). Then calculate and print the average of all test marks.
+
+#Taking input from user
+a=int(input())
+b=int(input())
+c=int(input())
+#Finding the average
+avg=(a+b+c) / 3
+print(avg)
